6c0a2d87966a10451793814bd781bf54dcd90e33
Bloodpack <kurierdienst.zimmermannqgooglemail.com>
2024-10-26 23:24:32 +0200

    testing strip "v" parameter from running version

--- update_semaphore_dry_run.sh
+++ update_semaphore_dry_run.sh
@@ -28,8 +28,8 @@ verbose_echo "Fetching latest releases..."
 RELEASE_INFO=$(curl -s https://api.github.com/repos/semaphoreui/semaphore/releases)
 
 # Extract the latest release tag and the download URL for amd64.deb
-RELEASE=$(echo "$RELEASE_INFO" | grep -m 1 '"tag_name"' | awk '{print substr($2, 2, length($2)-3)}')
-DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep -o "https://github.com/semaphoreui/semaphore/releases/download/${RELEASE}/semaphore_[^ ]*amd64.deb" | head -n 1)
+RELEASE=$(echo "$RELEASE_INFO" | grep -m 1 '"tag_name"' | awk '{print substr($2, 2, length($2)-3)}' | sed 's/^v//')
+DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep -o "https://github.com/semaphoreui/semaphore/releases/download/v${RELEASE}/semaphore_[^ ]*amd64.deb" | head -n 1)
 
 # Check if the release tag and download URL were successfully fetched
 if [ -z "$RELEASE" ] || [ -z "$DOWNLOAD_URL" ]; then
